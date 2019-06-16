db94ba0b98bb11dc0075d703f811d3d77b63b53e
Sowa <petergroeneweg@Peters-MBP.fritz.box>
2019-06-16 22:49:43 +0200

    Fix ArrayList iteration

--- android/src/main/java/flutter/plugins/contactsservice/contactsservice/ContactsServicePlugin.java
+++ android/src/main/java/flutter/plugins/contactsservice/contactsservice/ContactsServicePlugin.java
@@ -355,12 +355,12 @@ public class ContactsServicePlugin implements MethodCallHandler {
     ops.add(operation.build());
 
     //Phones
-    for(int i=1; i<contact.phones.length; i++){
+    for(int i=1; i<contact.phones.size(); i++){
       operation = ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI)
               .withSelection(ContactsContract.Data.CONTACT_ID + "=?" + " AND " + ContactsContract.Data.MIMETYPE + "=?" + " AND " + ContactsContract.CommonDataKinds.Phone.NUMBER + "=?",
-                      new String[]{String.valueOf(contact.identifier), Phone.CONTENT_ITEM_TYPE, String.valueOf(oldContact.phones[i].value)})
-              .withValue(CommonDataKinds.Phone.TYPE, Item.stringToPhoneType(contact.phones[i].label))
-              .withValue(CommonDataKinds.Phone.NUMBER, contact.phones[i].value);
+                      new String[]{String.valueOf(contact.identifier), Phone.CONTENT_ITEM_TYPE, String.valueOf(oldContact.phones.get(i).value)})
+              .withValue(CommonDataKinds.Phone.TYPE, Item.stringToPhoneType(contact.phones.get(i).label))
+              .withValue(CommonDataKinds.Phone.NUMBER, contact.phones.get(i).value);
       ops.add(operation.build());
     }
 
